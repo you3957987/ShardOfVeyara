7a1730df4c093e929a933ee1e834d3152b3a355c
HamYiHyeon <ehyun1165@gmail.com>
2025-09-30 03:04:38 +0900

    Farming System 00

---
+++ Source/AGSD/ACultivationPlot.h
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "ACultivationPlot.generated.h"
+
+UCLASS()
+class AGSD_API AACultivationPlot : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AACultivationPlot();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+	//오버랩 시작 시 작동할 함수
+	UFUNCTION()
+	void OnBeginOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+private:
+	//루트 컴포넌트
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = CultivationPlot, meta = (AllowPrivateAccess = "true"))
+	class USceneComponent* RootScene;
+	//콜리전 박스
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = CultivationPlot, meta = (AllowPrivateAccess = "true"))
+	class UBoxComponent* CollisionBox;
+};
